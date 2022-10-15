db95e86a5273fef2bc6e9ade47e4a8503f0f80c4
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-10-15 13:36:46 +0500

    header footer and scrollbar

--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -3,9 +3,7 @@ import { Container } from "react-bootstrap";
 const Hero = () => {
   return (
     <div className="hero">
-      <Container>
-        <h1>Hero</h1>
-      </Container>
+      <Container></Container>
     </div>
   );
 };
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,9 +1,78 @@
-// import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Stack, Text, Box, Flex, Heading, Link } from "@chakra-ui/react";
+import { BsFacebook, BsTelegram, BsTwitter, BsYoutube } from "react-icons/bs";
+import { FaMediumM, FaGithub, FaReddit } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="container-fluid p-4">
-      <p className="mb-0 text-center">&copy;Footer 2022</p>
+    <footer className="footer p-2">
+      <Container>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          gap={8}
+          justify="space-between"
+          py={8}
+        >
+          <Flex
+            direction={{ base: "column", md: "row" }}
+            maxW="500px"
+            gap={4}
+            align="center"
+          >
+            <Box>
+              <img
+                style={{ width: "100%", maxWidth: "100px" }}
+                src="assets/logo.png"
+                alt=""
+              />
+            </Box>
+            <Box>
+              <Heading size="sm" pb="2" textTransform="uppercase">
+                play to win rewards on holding
+              </Heading>
+              <Text textAlign="justify">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
+                quos iusto recusandae quasi reiciendis nostrum.
+              </Text>
+            </Box>
+          </Flex>
+
+          {/* icons */}
+          <Flex
+            alignSelf={{ base: "center", md: "flex-end" }}
+            direction="row"
+            gap="4"
+          >
+            <Link href="#">
+              <FaMediumM size={30} />
+            </Link>
+            <Link href="#">
+              <FaGithub size={30} />
+            </Link>
+            <Link href="#">
+              <BsTelegram size={30} />
+            </Link>
+            <Link href="#">
+              <BsTwitter size={30} />
+            </Link>
+            <Link href="#">
+              <BsYoutube size={30} />
+            </Link>
+            <Link href="#">
+              <FaReddit size={30} />
+            </Link>
+            <Link href="#">
+              <BsFacebook size={30} />
+            </Link>
+          </Flex>
+        </Stack>
+
+        <hr />
+
+        <Text as="p" mt={4} textAlign="center">
+          &copy;2022 Povo Production Limited
+        </Text>
+      </Container>
     </footer>
   );
 }
