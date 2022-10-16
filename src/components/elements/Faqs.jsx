1b01b8be8eaeb3f837159f7f067de6c839d136b3
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-10-16 19:48:49 +0500

    faqs

--- src/components/elements/Faqs.jsx
+++ src/components/elements/Faqs.jsx
@@ -1,9 +1,124 @@
 import { Container } from "react-bootstrap";
+import {
+  Box,
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+} from "@chakra-ui/react";
+
+const faqStyles = {
+  bg: "black",
+  border: "none",
+  py: 3,
+  my: 6,
+  borderRadius: "10px",
+  boxShadow: "1px 1px 20px 0 rgba(0,0,0,0.05)",
+};
 
 const Faqs = () => {
   return (
     <div className="faqs">
-      <Container></Container>
+      <Container>
+        <Box as="section" maxWidth="800px" mx="auto" py={16}>
+          <h1 className="fs-1 mb-5 fw-bold text-center">FAQ</h1>
+          <Accordion allowToggle>
+            <AccordionItem {...faqStyles}>
+              <h2>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left" fontSize="1.2rem">
+                    HOW TRANSACTIONS CAN BE PERFORMED?
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                Transactions in Povo can be made with a variety of digital
+                funds. This include your crypto currencies, NFT tokens. You can
+                purchase Povo tokens with fiat currency or swap with other
+                crypto assets, as well.
+              </AccordionPanel>
+            </AccordionItem>
+            <AccordionItem {...faqStyles}>
+              <h2>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left" fontSize="1.2rem">
+                    HOW CROSS CHAIN FUNCTIONALITIES WORK?
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                It works by allowing exchange of different cryptocurrencies
+                between different pairs. It basically works as a currency
+                exchange to facilitate your transfer/swap of crypto assets.
+              </AccordionPanel>
+            </AccordionItem>
+            <AccordionItem {...faqStyles}>
+              <h2>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left" fontSize="1.2rem">
+                    WHAT IS MULTIVERSE USE CASE IN BLOCKCHAIN GAME?
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                Multiverses can be used in gaming as an advanced feature to
+                enter in parallel universes to explore new area and levels
+                within the game.
+              </AccordionPanel>
+            </AccordionItem>
+            <AccordionItem {...faqStyles}>
+              <h2>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left" fontSize="1.2rem">
+                    HOW TRANSACTIONS CAN BE PERFORMED?
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                Transactions in Povo can be made with a variety of digital
+                funds. This include your crypto currencies, NFT tokens. You can
+                purchase Povo tokens with fiat currency or swap with other
+                crypto assets, as well.
+              </AccordionPanel>
+            </AccordionItem>
+            <AccordionItem {...faqStyles}>
+              <h2>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left" fontSize="1.2rem">
+                    HOW CROSS CHAIN FUNCTIONALITIES WORK?
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                It works by allowing exchange of different cryptocurrencies
+                between different pairs. It basically works as a currency
+                exchange to facilitate your transfer/swap of crypto assets.
+              </AccordionPanel>
+            </AccordionItem>
+            <AccordionItem {...faqStyles}>
+              <h2>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left" fontSize="1.2rem">
+                    WHAT IS MULTIVERSE USE CASE IN BLOCKCHAIN GAME?
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                Multiverses can be used in gaming as an advanced feature to
+                enter in parallel universes to explore new area and levels
+                within the game.
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </Box>
+      </Container>
     </div>
   );
 };
--- src/components/elements/Protected.jsx
+++ src/components/elements/Protected.jsx
@@ -1,9 +1,41 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Protected() {
   return (
     <div className="protected">
-      <Container></Container>
+      <h1 className="fs-1 text-center fw-bold mb-5">
+        How <span className="txt-red">Povo</span> is Rugpul protected
+      </h1>
+      <Container>
+        <Row>
+          <Col md={6} className="mb-4">
+            <ul>
+              <li>
+                Povo Presale is going held on UniCrypt which prevents rug-pull
+                by offering auto liquidity lock functionality
+              </li>
+              <li>
+                60% of raised funds in presale is locked into liquidity by
+                UniCrypt for 266 years
+              </li>
+              <li>Transaction fee is never above 7% max.</li>
+              <li>
+                Newly created LP tokens are locked in order to ensure
+                transparency.
+              </li>
+              <li>Smart Contract is audited by CertTik.</li>
+            </ul>
+          </Col>
+          <Col md={6} className="mb-4">
+            <img
+              src="assets/why.png"
+              className="mx-auto d-block"
+              style={{ width: "100%", maxWidth: "400px" }}
+              alt=""
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
